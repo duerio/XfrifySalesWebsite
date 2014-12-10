61f01ba65b27d627a54fb523ce8e6c829cfcc4fc
duerio <david.baudant@vmeretail.com>
2014-12-10 09:29:11 +0000

    David Grunt Revision tidy-up, before running grunt needs "npm install grunt-text-replace --save-dev"

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,14 +2,14 @@ var mozjpeg = require('imagemin-mozjpeg');
 module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        // Empty the dist and tmp directory (https://www.npmjs.org/package/grunt-contrib-clean)
-            clean: {
+        // Empty the dist and _tmp directory (https://www.npmjs.org/package/grunt-contrib-clean)
+        clean: {
                 dist: ['dist'],
-                tmp: ['tmp']
+                tmp: ['_tmp']
             },
 
-        // Concatenate all individual CSS files into tmp/css/tahoewww.css (https://www.npmjs.org/package/grunt-contrib-concat)
-            concat: {
+        // Concatenate all individual CSS files into _tmp/css/tahoewww.css (https://www.npmjs.org/package/grunt-contrib-concat)
+        concat: {
                 options: {
                     //stripBanners: true
                 },
@@ -33,24 +33,12 @@ module.exports = function (grunt) {
                         <link rel="stylesheet" href="assets/css/style.css" type="text/css" media="all" />
                         */
                     ],
-                    dest: 'tmp/css/<%= pkg.name %>.css',
-                    nonull: true
-                },
-
-                css2: {
-                    src: [
-                        'Startuply/Startuply_HTML/assets/css/font-lineicons.css',
-                        //'Startuply/Startuply_HTML/assets/css/style.css'
-
-                    ],
-                    dest: 'tmp/css/<%= pkg.name %>2.css',
+                    dest: '_tmp/css/<%= pkg.name %>.css',
                     nonull: true
                 },
 
                 js: { //DB: THAT DOESN'T SEEM TO WORK
                     src: [
-
-                        'Startuply/Startuply_HTML/assets/js/jquery-2.1.0.min.js',
                         'Startuply/Startuply_HTML/assets/js/bootstrap.min.js',
                         'Startuply/Startuply_HTML/assets/js/jquery.flexslider-min.js',
                         'Startuply/Startuply_HTML/assets/js/jquery.nav.js',
@@ -62,7 +50,9 @@ module.exports = function (grunt) {
                         'Startuply/Startuply_HTML/assets/js/jquery.validate.js',
                         'Startuply/Startuply_HTML/assets/js/toastr.min.js',
                         'Startuply/Startuply_HTML/assets/js/headhesive.min.js',
-                        'Startuply/Startuply_HTML/assets/js/scripts.min.js'
+                        'Startuply/Startuply_HTML/assets/mailchimp/js/mailing-list.js',
+                        'Startuply/Startuply_HTML/assets/js/scripts.min.js',
+
 
                         // Custom js file
                         //'src/js/custom.js'
@@ -79,38 +69,159 @@ module.exports = function (grunt) {
                 }
             },
 
-        // Minify the dist/css/tahoewww.css into dist/css/tahoewww.min.css (https://www.npmjs.org/package/grunt-contrib-cssmin)
-            cssmin: {
-                my_target: {
-                    files: [{ //DB: IS THAT NEEDED?
+        // Copy various files to _tmp  (https://www.npmjs.org/package/grunt-contrib-copy)
+        copy: {
+            tmp: {
+                files: [
+                    {//fonts
                         expand: true,
-                        cwd: 'Startuply/Startuply_HTML/assets/css/',
-                        src: ['*.css', '!*.min.css', '!font-lineicons.css'],
-                        dest: 'dist/css/',
-                        ext: '.min.css'
-                    }]
-                },
-                tahoe: {
-                    src: 'tmp/css/<%= pkg.name %>.css',
-                    dest: 'dist/css/<%= pkg.name %>.min.css'
-                },
-                tahoe2: { //DB: NOT DOING ANYTHING AT THE MINUTE
-                    src: 'tmp/css/<%= pkg.name %>2.css',
-                    dest: 'dist/css/<%= pkg.name %>2.min.css'
-                }
+                        cwd: 'Startuply/Startuply_HTML/assets/fonts/',
+                        src: '*',
+                        dest: '_tmp/fonts'
+                    },
+                    {//\css exception
+                        expand: true,
+                        flatten:true,
+                        src: 'Startuply/Startuply_HTML/assets/css/font-lineicons.css',
+                        dest: '_tmp/css'
+                    },
+                    {//custom images
+                        expand: true,
+                        cwd: 'src/img/',
+                        src: '**/*',
+                        dest: '_tmp/img/'
+                    },
+                ],
             },
+            js: { //javascript
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        cwd: 'Startuply/Startuply_HTML/assets/js/',
+                        src: ['jquery-1.11.0.min.js', 'jquery-2.1.0.min.js','jquery-migrate-1.2.1.min.js', 'bootstrap/js/bootstrap.min.js'],
+                        dest: '_tmp/js/'
+                    },
+                    {
+                        expand: true,
+                        flatten: true,
+                        cwd: 'Startuply/Startuply_HTML/assets/js/',
+                        src: ['jquery.*.js' ],
+                        dest: '_tmp/js/'
+                    },
+                    {
+                        expand: true,
+                        flatten: true,
+                        cwd: 'Startuply/Startuply_HTML/assets/js/',
+                        src: ['jquery.*.js','bootstrap.min.js','waypoints*.js','toastr.min.js*','headhesive.min.js','scripts.js' ],
+                        dest: '_tmp/js/'
+                    },
+                    {
+                        expand: true,
+                        flatten: true,
+                        cwd: 'Startuply/Startuply_HTML/assets/mailchimp/js/',
+                        src: ['mailing-list.js' ],
+                        dest: '_tmp/js/'
+                    },
 
-        // Minify the Startuply/Startuply_HTML/assets/*.js into dist/js/ (https://www.npmjs.org/package/grunt-contrib-uglify)
-            uglify: {
+                ],
+            },
+            final: { // all except tahoewww.css and JS.
+                expand: true,
+                cwd: '_tmp',
+                src: ['*/**','!**/tahoewww.css', '!js/*', '!img/**'],
+                dest: 'dist'
+            }
+
+        },
+
+        // Uglify the _tmp/js/*.js into dist/js/ (https://www.npmjs.org/package/grunt-contrib-uglify)
+        uglify: {
                     my_target: {
                         files: [{
                             expand: true,
-                            cwd: 'Startuply/Startuply_HTML/assets/js',
+                            cwd: '_tmp/js',
                             src: '**/*.js',
                             dest: 'dist/js/'
                         }]
                     }
                 },
+
+        // Replace individual CSS references with tahoewww.min.css instead into the _tmp/index_tmp.html (https://www.npmjs.org/package/grunt-processhtml)
+        processhtml: { //DB: Added To replace individual CSS ref with tahoewww.min.css instead in the dist/index.html
+            dist: {
+                files: {
+                    '_tmp/index.html': ['src/index.html']
+                }
+            }
+        },
+
+        // Minify the _tmp/css/tahoewww.css into _tmp/css/tahoewww.min.css (https://www.npmjs.org/package/grunt-contrib-cssmin)
+        cssmin: {
+            minify: {
+                expand: true,
+                cwd: '_tmp/css/',
+                src: ['*.css', '!*.min.css', '!font-lineicons.css'],
+                dest: '_tmp/css/',
+                ext: '.min.css'
+            }
+        },
+
+        // Minify images in '_tmp/img/' to 'dist/img/' (https://www.npmjs.org/package/grunt-contrib-imagemin)
+        imagemin: {                          // Task
+            dynamic: {                         // Another target
+                files: [{
+                    expand: true,                  // Enable dynamic expansion
+                    cwd: '_tmp/img/',                   // Src matches are relative to this path
+                    src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+                    dest: 'dist/img/'                  // Destination path prefix
+                }]
+            }
+        },
+
+        // Replace various paths in the *.html files (https://www.npmjs.org/package/grunt-text-replace)
+        replace: {
+            img: {
+                src: ['_tmp/*.html'],             // source files array (supports minimatch)
+                overwrite: true,                 // overwrite matched source files
+                replacements: [{
+                    from: 'frontend/onepage/img/',                   // string replacement
+                    to: 'img/'
+                }, ]
+            },
+            js: {
+                src: ['_tmp/*.html'],             // source files array (supports minimatch)
+                overwrite: true,                 // overwrite matched source files
+
+                replacements: [
+                    {
+                        from: 'src="../Startuply/Startuply_HTML/assets/js/',                   // string replacement
+                        to: 'src="js/'
+                    },
+                    {
+                        from: 'src="../Startuply/Startuply_HTML/assets/mailchimp/js/',                   // string replacement
+                        to: 'src="js/'
+                    },
+                ]
+
+            }
+
+        },
+
+        // Minify the 'tmp/index.html' into 'dist/index.html' (https://www.npmjs.org/search?q=minify-html)
+        minifyHtml: {
+            options: {
+                //cdata: true,        // KEEP CDATA from scripts
+                loose: true         // KEEP retain one whitespace (doesn't work right without here)
+            },
+            dist: {
+                files: {
+                    'dist/index.html': '_tmp/index.html'
+                }
+            }
+        },
+
+        // ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
         // Compress in gzip format the dist/ directory content into the dist2/ (https://www.npmjs.org/package/grunt-contrib-compress)
             compress: { //DB: NOT USED AT THE MOMENT
@@ -123,32 +234,6 @@ module.exports = function (grunt) {
                     src: ['**/*'],
                     dest: 'dist2/'
                 }
-            },
-
-        // Copy 'Startuply/Startuply_HTML/assets/fonts/' to 'dist/fonts/ (https://www.npmjs.org/package/grunt-contrib-copy)
-            copy: {
-                fonts: {
-                    expand: true,
-                    cwd: 'Startuply/Startuply_HTML/assets/fonts/',
-                    src: '**/*',
-                    dest: 'dist/fonts/'
-                }/*
-                css: {
-                    expand: true,
-                    cwd: 'src/css/',
-                    src: '*.css',
-                    dest: 'dist/css/'
-                },/*
-                js: {
-                    expand: true,
-                    cwd: 'Startuply/Startuply_HTML/assets/js/',
-                    src: '*.js',
-                    dest: 'dist/assets/js/'
-                }*/
-
-
-
-
             },
 
         // A predefined tasks whenever watched file patterns are added, changed or deleted. (https://www.npmjs.org/package/grunt-contrib-watch)
@@ -180,41 +265,6 @@ module.exports = function (grunt) {
             }
         },
 
-        // Minify images in 'src/img/' to 'dist/img/' (https://www.npmjs.org/package/grunt-contrib-imagemin)
-            imagemin: {                          // Task
-                dynamic: {                         // Another target
-                    files: [{
-                        expand: true,                  // Enable dynamic expansion
-                        cwd: 'src/img/',                   // Src matches are relative to this path
-                        src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
-                        dest: 'dist/img/'                  // Destination path prefix
-                    }]
-                }
-            },
-            //imagemin: {                          // Task
-            //    dynamic: {                         // Another target
-            //        files: [{
-            //            expand: true,                  // Enable dynamic expansion
-            //            cwd: 'Startuply/Startuply_HTML/assets/img/',                   // Src matches are relative to this path
-            //            src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
-            //            dest: 'dist/img/'                  // Destination path prefix
-            //        }]
-            //    }
-            //},
-
-        // Minify the 'tmp/index.html' into 'dist/index.html' (https://www.npmjs.org/search?q=minify-html)
-            minifyHtml: {
-                options: {
-                    //cdata: true,        // KEEP CDATA from scripts
-                    loose: true         // KEEP retain one whitespace
-                },
-                dist: {
-                    files: {
-                        'dist/index.html': 'tmp/index.html'
-                    }
-                }
-            },
-
         // Remove unused CSS style (https://www.npmjs.org/package/grunt-uncss)
             uncss: { //DB: TRIED IT BUT IT IS CAUSING PROBLEMS, UNCSS CAN'T DETECT STYLES ADDED
                      //    BY USER INTERACTION WITH THE PAGE SO EVERYTHING THAT IS NOT RECOGNISED
@@ -224,27 +274,10 @@ module.exports = function (grunt) {
                         ignore: ['#added_at_runtime', '.created_by_jQuery']
                     },
                     files: {
-                        'tmp/css/<%= pkg.name %>.css': ['src/index.html']
+                        'dist/css/<%= pkg.name %>.min.css': ['src/index.html']
                     }
                 }
             },
-
-        // Replace individual CSS references with tahoewww.min.css instead into the tmp/index_tmp.html (https://www.npmjs.org/package/grunt-processhtml)
-            processhtml: { //DB: Added To replace individual CSS ref with tahoewww.min.css instead in the dist/index.html
-                dist: {
-                    files: {
-                        'tmp/index.html': ['src/index.html']
-                    }
-                }
-            },
-            //DB: REMOVED SECTION BELOW, DON'T THINK IT WAS NEEDED
-            //processhtml: {
-            //    dist: {
-            //        files: {
-            //            'dist/index2.html': ['dist/srcindex.html']
-            //        }
-            //    }
-            //},
 
         // Convert a set of images into a spritesheet and corresponding CSS variables (https://www.npmjs.org/package/grunt-spritesmith)
             spriteGenerator: { //DB: PROBABLY BEST IMPLEMENTED ONCE THE WEBSITE IS CLOSE TO FINISHED
@@ -267,23 +300,27 @@ module.exports = function (grunt) {
                 destCSS: 'src/css/custom2.css',
                 'imgPath': '../images/spritesheet.png'
             }
-        }
+        },
+
     });
 
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-processhtml');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-text-replace');
+    grunt.loadNpmTasks('grunt-minify-html');
+    // ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     grunt.loadNpmTasks('grunt-contrib-watch');
     //grunt.loadNpmTasks('grunt-s3');
     grunt.loadNpmTasks('grunt-aws');
     grunt.loadNpmTasks('grunt-contrib-compress');
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
-    grunt.loadNpmTasks('grunt-minify-html');
     grunt.loadNpmTasks('grunt-uncss');
-    grunt.loadNpmTasks('grunt-processhtml');
     grunt.loadNpmTasks('grunt-spritesmith');
+
     //grunt.loadNpmTasks('grunt-sprite-generator');
 
     //grunt.registerTask('default', [ 'clean', 'copy', 'concat:css', 'cssmin', 'concat:js', 'concat:js4ie', 'uglify:js', 'clean:tmp' ]);
@@ -291,7 +328,10 @@ module.exports = function (grunt) {
 
     //DB: Original grunt.registerTask was including the "Sprite task which was currently overwriting the custom.css so I've disabled it for now
     //grunt.registerTask('default', [ 'clean', 'sprite', 'copy', 'concat:css', 'cssmin:tahoe', 'cssmin:tahoe2', 'uglify', 'minifyHtml', 'imagemin', 'clean:tmp' ]);
-    grunt.registerTask('default', [ 'clean', 'copy', 'concat:css', 'cssmin:tahoe', 'cssmin:tahoe2', 'uglify', 'processhtml', 'minifyHtml', 'imagemin', 'clean:tmp' ]);
+    //grunt.registerTask('default', [ 'clean', 'concat:css', 'concat:js', 'copy:tmp', 'copy:js', 'cssmin:tahoe', 'uglify', 'processhtml','cssmin:tahoe' , 'imagemin',  'replace', 'minifyHtml', 'copy:final' ]);
+    grunt.registerTask('default', [ 'clean', 'concat:css', 'concat:js', 'copy:tmp', 'copy:js', 'uglify', 'processhtml','cssmin' , 'imagemin',  'replace', 'minifyHtml', 'copy:final' ]);
+
     grunt.registerTask('deploy', [ 's3' ]);
+
 
 };
